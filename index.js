5f25aee9796607891067cadc993ba97d1048a76a
VickyDana <uviwedana09@gmail>
2023-09-29 09:56:23 +0200

    updates

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 const songRoutes = require('./routes/songsroutes');
-=======
-const trackRoutes = require('./routes/songsroutes');
->>>>>>> 69fbf3facd62f98b43eec4bba7d27c258b8f4108
 const PORT = process.env.PORT || 3000;
 const app = express();
 const cors= require("cors")
@@ -23,11 +19,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/newMusic',
     .catch((e)=> console.log("Couldn't connect to DB: ", e));
 
 
-<<<<<<< HEAD
 app.use('/api', songRoutes);
-=======
-app.use('/api', trackRoutes);
->>>>>>> 69fbf3facd62f98b43eec4bba7d27c258b8f4108
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
